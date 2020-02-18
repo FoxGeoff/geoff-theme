3c1c6fae99330f4c0f4a8e5ff3fac2d7ea807ba0
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-02-17 16:44:46 -0800

    Add site-sidebar and  site-content

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
+</div> <!-- end of site-content -->
+</div> <!-- end of site-container -->
+
 <?php wp_footer(); ?>
 </body>
 </html> 
--- header.php
+++ header.php
@@ -8,3 +8,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+    <header id='site-header'>
+        <h1><?php bloginfo('title') ?></h1>
+    </header>
+
+    <div id='site-container'>
+        <div id='site-sidebar'>
+            Sidebar goes here
+        </div>
+
+        <div id='site-content>'
