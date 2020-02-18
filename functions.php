7e0074b4b9e54816afa4cfa9be0a08a3ba825b27
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-02-17 17:14:10 -0800

    Add functions 'enqueue'

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
     add_theme_support('title-tag');
+
+/**
+ * Enqueue scripts and styles.
+ */
+    function geoff_theme_scripts() {
+        wp_enqueue_style( 'geoff-theme-styles',get_stylesheet_uri() );
+    }
+
+    add_action( 'wp_enqueue_scripts', 'geoff_theme_scripts' );
 ?>
